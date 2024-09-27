ecbddc33f3937c894e7d8bb3bdab5d9d905aeaae
rayansb1 <rayanbawazeer1@hotmail.com>
2024-09-27 17:11:17 +0300

    Ready Data Model 😎

--- lib/componants/character_card.dart
+++ lib/componants/character_card.dart
@@ -1,9 +1,12 @@
+import 'package:appch/style/styled_text.dart';
 import 'package:appch/style/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:appch/models/character.dart';
 
 class CharacterCard extends StatelessWidget {
   const CharacterCard(this.character, {super.key});
-  final String character;
+  final Character character;
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -11,8 +14,19 @@ class CharacterCard extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Row(
           children: [
-            Text(character),
+            Image.asset(
+              'assets/vocation/${character.vocation.image}',
+              width: 50,
+            ),
+            const SizedBox(width: 20),
             const Expanded(child: SizedBox()),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                StyledHeading(character.name),
+                StyledText(character.vocation.title),
+              ],
+            ),
             IconButton(
               onPressed: () {},
               icon: Icon(Icons.arrow_forward, color: AppColors.textColor),
